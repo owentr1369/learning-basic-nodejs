1d16f6fd0978e821f183a10de270003666ee23d5
TranTam <tamvaa1306@gmail.com>
2022-09-30 22:35:05 +0700

    env variables in node.js

---
+++ basic-npm-begin/env.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+
+console.log(process.env.NAME);
+console.log(process.env.PROFESSION);
+// console.log(process.env.COURSE);
+
+console.log("I'm starting a new series on ", process.env.COURSE);
--- basic-npm-begin/index.js
+++ basic-npm-begin/index.js
@@ -2,5 +2,4 @@ console.log("Hello this is my node.js project!");
 console.log("We'll cover this node.js with command line");
 
 // process.exit(0);
-
-process.exit = 1;
+// process.exitCode = 1;
